7607da8b5d5b6f77e9e2ca7703d941eca329b52e
Yap Jun Cheng <cheng03@graduate.utm.my>
2024-12-28 01:38:23 +0800

    Tabbar button styling

---
+++ components/CustomTabbar.tsx
@@ -0,0 +1,118 @@
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import React from "react"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+
+interface CustomTabBarProps {
+  state: {
+    index: number
+    routes: Array<{
+      key: string
+      name: string
+    }>
+  }
+  descriptors: any
+  navigation: any
+}
+
+const TabBar: React.FC<CustomTabBarProps> = ({ state, descriptors, navigation }) => {
+
+  type IconName = "home" | "cart" | "message" | "status" | "profile";
+  
+  const icons: Record<IconName, (props: { color: string }) => JSX.Element> = {
+    home: (props) => <FontAwesome size={28} name="home" color={props.color} />,
+    cart: (props) => <FontAwesome size={24} name="shopping-cart" color={props.color} />,
+    message: (props) => <AntDesign size={22} name="message1" color={props.color} />,
+    status: (props) => <FontAwesome size={24} name="star" color={props.color} />,
+    profile: (props) => <Ionicons  size={24} name="person" color={props.color} />,
+  }
+
+  const primaryColor = '#969696';
+  const secondaryColor = "#FF9001";
+
+  return (
+    
+    <View style={styles.container}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+            // console.log("Routes", route);
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+          key={route.name}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.tab}
+          >
+            {icons[route.name as IconName]({color: isFocused ? secondaryColor : primaryColor})}
+            <Text style={{ color: isFocused ? secondaryColor : primaryColor }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+export default TabBar
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 24,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    padding: 12,
+    marginHorizontal: 16,
+    borderRadius: 20,
+    borderCurve: "continuous",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.1,
+    elevation: 4,
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  label: {
+    fontSize: 12,
+    marginTop: 4,
+  },
+})
